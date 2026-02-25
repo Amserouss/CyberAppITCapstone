0a5fabe2c020044749c96c2e3fb86186e86ae0d5
EmilioB <emiliobproper@gmail.com>
2026-02-24 19:08:32 -0500

    WIP 2/23/2026 Add result screen | add return to home during quiz screens | Research Questions

---
+++ app/src/main/java/com/example/cyberapp/InterDevQuiz.java
@@ -0,0 +1,154 @@
+package com.example.cyberapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InterDevQuiz extends AppCompatActivity {
+    private TextView questionText, questionCounter;
+    private RadioGroup radioGroup;
+    private RadioButton answer1, answer2, answer3;
+    private Button nextButton;
+
+    private List<QuizQuestionLogic> questions;
+    private int currentIndex = 0;
+    //4
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inter_quiz2);
+
+        questionText = findViewById(R.id.questionText);
+        questionCounter = findViewById(R.id.questionCounter);
+        radioGroup = findViewById(R.id.radioGroup);
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        answer3 = findViewById(R.id.answer3);
+        nextButton = findViewById(R.id.nextButton);
+
+        questions = new ArrayList<>();
+
+        // determine order for correct answer in each question
+        questions.add(new QuizQuestionLogic(
+                getString(R.string.interDev_question_1),
+                new String[]{
+                        getString(R.string.interDevQ1A1Answer),
+                        getString(R.string.interDevQ1A2Answer),
+                        getString(R.string.interDevQ1A3Answer),
+                },
+                0 // correct answer index
+        ));
+
+        questions.add(new QuizQuestionLogic(
+                getString(R.string.interDev_question_2),
+                new String[]{
+                        getString(R.string.interDevQ2A1Answer),
+                        getString(R.string.interDevQ2A2Answer),
+                        getString(R.string.interDevQ2A3Answer)
+                },
+                1
+        ));
+
+        questions.add(new QuizQuestionLogic(
+                getString(R.string.interDev_question_3),
+                new String[]{
+                        getString(R.string.interDevQ3A1Answer),
+                        getString(R.string.interDevQ3A2Answer),
+                        getString(R.string.interDevQ3A3Answer)
+                },
+                1
+        ));
+
+        questions.add(new QuizQuestionLogic(
+                getString(R.string.interDev_question_4),
+                new String[]{
+                        getString(R.string.interDevQ4A1Answer),
+                        getString(R.string.interDevQ4A2Answer),
+                        getString(R.string.interDevQ4A3Answer)
+                },
+                1
+        ));
+
+        questions.add(new QuizQuestionLogic(
+                getString(R.string.interDev_question_5),
+                new String[]{
+                        getString(R.string.interDevQ5A2Answer),
+                        getString(R.string.interDevQ5A3Answer),
+                        getString(R.string.interDevQ5A1Answer),
+                },
+                1
+        ));
+
+        loadQuestion(currentIndex);//determine where this goes and update
+//6
+        nextButton.setOnClickListener(v -> {
+
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+
+            if (selectedId == -1) {
+                Toast.makeText(this, "Please select an answer", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int selectedIndex = radioGroup.indexOfChild(
+                    findViewById(selectedId));
+
+            questions.get(currentIndex).setSelectedAnswer(selectedIndex);
+
+            currentIndex++;
+
+            if (currentIndex < questions.size()) {
+                loadQuestion(currentIndex);
+            } else {
+                calculateScore();
+            }
+        });
+
+    }
+
+    //7
+    private void calculateScore() {
+
+        int score = 0;
+
+        for (QuizQuestionLogic q : questions) {
+            if (q.getSelectedAnswer() == q.getCorrectAnswer()) {
+                score++;
+            }
+        }
+
+        Toast.makeText(this,
+                "You scored " + score + " out of " + questions.size(),
+                Toast.LENGTH_LONG).show();
+    }
+
+
+    //5
+    private void loadQuestion(int index) {
+
+        QuizQuestionLogic question = questions.get(index);
+
+        questionCounter.setText("Question " + (index + 1) + " of " + questions.size());
+
+        questionText.setText(question.getQuestionText());
+
+        answer1.setText(question.getAnswers()[0]);
+        answer2.setText(question.getAnswers()[1]);
+        answer3.setText(question.getAnswers()[2]);
+
+        radioGroup.clearCheck();
+
+        if (question.getSelectedAnswer() != -1) {
+            radioGroup.check(radioGroup.getChildAt(
+                    question.getSelectedAnswer()).getId());
+        }
+    }
+}
--- app/src/main/java/com/example/cyberapp/IntermediateHome.java
+++ app/src/main/java/com/example/cyberapp/IntermediateHome.java
@@ -1,6 +1,49 @@
 package com.example.cyberapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class IntermediateHome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.intermediate_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.intermediateHomeId), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void buttonClick(View v) {
+        Intent intent = null;
+
+        int id = v.getId();
+
+        if (id == R.id.webQuizInterId) {
+            intent = new Intent(this, BegWebQuiz.class);
+
+        } else if (id == R.id.devQuizInterId) {
+            intent = new Intent(this, BegDevQuiz.class);
+
+        } else if (id == R.id.cyberQuizInterId) {
+            intent = new Intent(this, BegCyberQuiz.class);
+
+        } else if (id == R.id.returnButtonId) {
+            intent = new Intent(this, MainActivity.class);
+
+        } else {
+            intent = new Intent(this, ErrorPage.class);
+        }
+
+        startActivity(intent);
+    }
 }
