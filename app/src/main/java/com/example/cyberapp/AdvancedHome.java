f08f0fb77875097adc084759b8c7a56d2b138d5c
EmilioB <emiliobproper@gmail.com>
2026-02-24 20:01:29 -0500

    WIP 2/24/2026 Add result screen | add return to home during quiz screens | Research Questions

--- app/src/main/java/com/example/cyberapp/AdvancedHome.java
+++ app/src/main/java/com/example/cyberapp/AdvancedHome.java
@@ -1,6 +1,49 @@
 package com.example.cyberapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class AdvancedHome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.advanced_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.advancedHomeId), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void buttonClick(View v) {
+        Intent intent = null;
+
+        int id = v.getId();
+
+        if (id == R.id.webQuizAdvId) {
+            intent = new Intent(this, BegWebQuiz.class);
+
+        } else if (id == R.id.devQuizAdvId) {
+            intent = new Intent(this, BegDevQuiz.class);
+
+        } else if (id == R.id.cyberQuizAdvId) {
+            intent = new Intent(this, BegCyberQuiz.class);
+
+        } else if (id == R.id.returnButtonId) {
+            intent = new Intent(this, MainActivity.class);
+
+        } else {
+            intent = new Intent(this, ErrorPage.class);
+        }
+
+        startActivity(intent);
+    }
 }
