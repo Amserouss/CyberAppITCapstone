92e904268b96a6cc5cbb3def00e5c1eac0e8b695
EmilioB <emiliobproper@gmail.com>
2026-02-27 11:42:01 -0500

    WIP 2/24/2026 Add result screen | add return to home during quiz screens | Research Questions | finish error page

--- app/src/main/java/com/example/cyberapp/ErrorPage.java
+++ app/src/main/java/com/example/cyberapp/ErrorPage.java
@@ -1,6 +1,26 @@
 package com.example.cyberapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ErrorPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_page);
+
+    }
+
+    public void buttonClick(View v) {
+        Intent intent = null;
+
+        int id = v.getId();
+
+        if (id == R.id.returnButtonId) {
+            intent = new Intent(this, ErrorPage.class);
+        }
+    }
 }
